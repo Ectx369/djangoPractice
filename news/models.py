75ac416b6eefcade6016d8dc6400be22f572478e
Ussen <freefall.pdf@gmail.com>
2022-11-06 03:37:14 +0500

    day4 25

--- news/models.py
+++ news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class News(models.Model):
@@ -6,12 +7,15 @@ class News(models.Model):
     content = models.TextField(blank=True, verbose_name='Соддержание')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикаций')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
-    photo = models.ImageField(upload_to='photos/%Y%m%d', verbose_name='Фото', blank=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано?')
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('viev_news', kwargs={'news_id': self.pk})
 
     class Meta:
         verbose_name = 'Новость'
@@ -22,8 +26,12 @@ class News(models.Model):
 class Category(models.Model):
     title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование категорий')
 
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'category_id': self.pk})
+
     def __str__(self):
         return self.title
+
     class Meta:
         verbose_name = 'Категорий'
         verbose_name_plural = 'Категорий'
--- news/views.py
+++ news/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+
 from news.models import News, Category
+
+from news.forms import NewsForm
 
 
 def index(request):
@@ -14,7 +16,23 @@ def index(request):
 
 
 def get_category(request, category_id):
-    news = News.objects.filter(category_id = category_id)
+    news = News.objects.filter(category_id=category_id)
     categories = Category.objects.all()
-    category = Category.objects.get(pk=category_id)
-    return render(request, 'news/category.html', {'news': news, 'categories':categories})
+    return render(request, 'news/category.html', {'news': news, 'categories': categories})
+
+
+def viev_news(request, news_id):
+    news_item = get_object_or_404(News, pk=news_id)
+    return render(request, 'news/view_news.html', {'news_item': news_item})
+
+
+def add_news(request):
+    if request.method == 'POST':
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            # news = News.objects.create(**form.cleaned_data)
+            news = form.save()
+            return redirect(news)
+    else:
+        form = NewsForm()
+    return render(request, 'news/add_news.html', {'form': form})
